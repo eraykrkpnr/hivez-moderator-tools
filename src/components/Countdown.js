08c57ec2af2d72386bc684ed599f7956c38d86a9
Eray Kırkpınar <eraykirkpinar@gmail.com>
2024-06-13 15:53:59 +0300

    timer

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,21 +1,20 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function Countdown() {
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
-  const [startTime, setStartTime] = useState(0);
+  const startTimeRef = useRef(0); // Use useRef to store the start time
 
   const setTime = () => {
-    setStartTime(Date.now());
-    console.log(startTime);
+    startTimeRef.current = Date.now();
+    console.log(startTimeRef.current);
   };
 
   const getTime = () => {
-    const time = Date.now() - startTime;
+    const time = Date.now() - startTimeRef.current;
     console.log("asd");
-    if (startTime != 0) {
+    if (startTimeRef.current !== 0) {
       setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
       setMinutes(Math.floor((time / 1000 / 60) % 60));
       setSeconds(Math.floor((time / 1000) % 60));
@@ -30,9 +29,10 @@ function Countdown() {
 
   return (
     <div>
-      <b>{hours}</b>
-      <b>{minutes}</b>
-      <b>{seconds}</b>
+      <b>{hours.toString().padStart(2, "0")}:</b>
+      <b>{minutes.toString().padStart(2, "0")}:</b>
+      <b>{seconds.toString().padStart(2, "0")}</b>
+      <br />
       <button onClick={setTime}>Set Time</button>
     </div>
   );
