c1a1f098991d77e709c109977dab59991eb335fb
Eray Kırkpınar <eraykirkpinar@gmail.com>
2024-07-26 13:03:44 +0300

    Kişi sayıları eklendi

--- src/components/EkipList.js
+++ src/components/EkipList.js
@@ -122,6 +122,18 @@ function EkipList() {
             ? uncertainNames.join(" - ")
             : "Hiçbir isim belirsiz değil."}
         </div>
+        <div>
+          <b>
+            Kesin Gelecek Kişi Sayısı:{" "}
+            {selectedNames.length - uncertainNames.length}
+          </b>
+        </div>
+        <div>
+          <b>Gelme İhtimali Olan Kişi Sayısı: {uncertainNames.length}</b>
+        </div>
+        <div>
+          <b>Toplam Kişi Sayısı: {selectedNames.length}</b>
+        </div>
       </div>
     </div>
   );
